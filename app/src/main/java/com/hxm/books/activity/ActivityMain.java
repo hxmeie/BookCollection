02b06cdca705c946432b8cb208b4c7569e2f011a
hexiaomeng <1247535723@qq.com>
2016-01-25 16:40:41 +0800

    更新

--- app/src/main/java/com/hxm/books/activity/ActivityMain.java
+++ app/src/main/java/com/hxm/books/activity/ActivityMain.java
@@ -1,12 +1,17 @@
 package com.hxm.books.activity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import com.hxm.books.R;
 
 /**
  * Created by hxm on 2016/1/13.
  */
-public class ActivityMain extends BaseActivity {
+public class ActivityMain extends BaseActivity implements RadioGroup.OnCheckedChangeListener{
+
+    private RadioGroup menuGroup;
+    private RadioButton btnBookshelf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,32 @@ public class ActivityMain extends BaseActivity {
     }
 
     public void initView(){
-        initOnlyTitle(stringId(this,R.string.activity_main_title));
+        initOnlyTitle(stringId(this,R.string.title_bookshelf));
+        menuGroup= (RadioGroup) findViewById(R.id.menu_radio_group);
+        btnBookshelf= (RadioButton) findViewById(R.id.btn_bookshelf);
+        btnBookshelf.setChecked(true);
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.btn_bookshelf:
+
+
+                break;
+            case R.id.btn_classify:
+
+
+                break;
+            case R.id.btn_discovery:
+
+
+                break;
+            case R.id.btn_mine:
+
+
+                break;
+        }
     }
 }
